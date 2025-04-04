ba7f97e635f3c4aeb7226e551bfb5959c34b0ad5
johnsonoklii <johnsonoklii@163.com>
2025-04-05 00:03:16 +0800

    fix: gotest

--- scripts/make-rules/golang.mk
+++ scripts/make-rules/golang.mk
@@ -76,7 +76,7 @@ go.test: tools.verify.go-junit-report
 	@echo "===========> Run unit test"
 	@set -o pipefail;$(GO) test -race -cover -coverprofile=$(OUTPUT_DIR)/coverage.out \
 		-timeout=10m -shuffle=on -short -v `go list ./...|\
-		egrep -v $(subst $(SPACE),'|',$(sort $(EXCLUDE_TESTS)))` 2>&1 | \
+		grep -Ev $(subst $(SPACE),'|',$(sort $(EXCLUDE_TESTS)))` 2>&1 | \
 		tee >(go-junit-report --set-exit-code >$(OUTPUT_DIR)/report.xml)
 	@sed -i '/mock_.*.go/d' $(OUTPUT_DIR)/coverage.out # remove mock_.*.go files from test coverage
 	@$(GO) tool cover -html=$(OUTPUT_DIR)/coverage.out -o $(OUTPUT_DIR)/coverage.html
